2563df6a98ba50afa144a84f30ed20a99d7a95bf
unknown <diegos.araujo4@gmail.com>
2025-01-12 03:57:34 -0300

    Adding more tests

--- main_test.go
+++ main_test.go
@@ -4,6 +4,9 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
+
+	"github.com/go-redis/redismock/v9"
 )
 
 func TestResponder(t *testing.T) {
@@ -24,5 +27,30 @@ func TestResponder(t *testing.T) {
 	expected := "OK"
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
+
+func TestMainFunction(t *testing.T) {
+	client, mock := redismock.NewClientMock()
+	redisClient = client
+
+	// Start the server in a goroutine
+	go func() {
+		main()
+	}()
+	time.Sleep(1 * time.Second) // Give the server a second to start
+
+	mock.ExpectIncr("ip:::1").SetVal(int64(1))
+	mock.ExpectExpire("ip:::1", time.Duration(300*time.Second)).SetVal(true)
+
+	// Test if the server is running and responding
+	resp, err := http.Get("http://localhost:8080")
+	if err != nil {
+		t.Fatalf("Failed to send GET request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Expected status code %v, got %v", http.StatusOK, resp.StatusCode)
 	}
 }
